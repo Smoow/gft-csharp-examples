e25a9615034a1d35c72f82420e9eef8e36ff51cd
Smoow <pedrohcm5@gmail.com>
2021-01-29 19:16:21 -0300

    Revisao updated - Inserir Alunos e Listar Alunos

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -6,6 +6,62 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            int indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    // Adicionar aluno
+                    case "1":
+                        Console.WriteLine("Informe o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno: ");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else 
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
+                        break;
+
+                    // Listar alunos
+                    case "2":
+                        foreach (var i in alunos)
+                        {
+                            if (i.Nome != null)
+                            {
+                                Console.WriteLine($"Aluno: {i.Nome}, Nota {i.Nota}");
+                            }
+                        }
+                        break;
+
+                    // Calcular media geral
+                    case "3":
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
             Console.WriteLine("Informe a opcao desejada: ");
             Console.WriteLine("1 - Insira um novo aluno");
             Console.WriteLine("2 - Listar alunos");
@@ -14,37 +70,8 @@ namespace Revisao
             Console.WriteLine();
 
             string opcaoUsuario = Console.ReadLine();
-
-            while (opcaoUsuario.ToUpper() != "X") 
-            {
-                switch (opcaoUsuario)
-                {
-                    // Adicionar aluno
-                    case "1":
-                        break;
-
-                    // Listar alunos
-                    case "2":
-                        break;
-                        
-                    // Calcular media geral
-                    case "3":
-                        break;
-
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-                
-                Console.WriteLine("Informe a opcao desejada: ");
-                Console.WriteLine("1 - Insira um novo aluno");
-                Console.WriteLine("2 - Listar alunos");
-                Console.WriteLine("3 - Calcular media geral");
-                Console.WriteLine("X - Sair");
-                Console.WriteLine();
-
-                opcaoUsuario = Console.ReadLine();
-            }
-
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
